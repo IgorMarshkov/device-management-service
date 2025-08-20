ef5ce62b5631cc389da52bb378e57d792efe097b
Igor Marshkov <imarshkov@workfusion.com>
2025-08-20 19:15:23 +0200

    Init base project

---
+++ src/main/java/com/interview/DeviceManagementApp.java
@@ -0,0 +1,16 @@
+package com.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot main class to run the application.
+ */
+@SpringBootApplication
+public class DeviceManagementApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagementApp.class, args);
+    }
+
+}
