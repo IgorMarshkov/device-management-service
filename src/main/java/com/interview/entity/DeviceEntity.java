fc46595dd12ce378422c84099023b97f2e16b8f9
Igor Marshkov <imarshkov@workfusion.com>
2025-08-20 19:56:53 +0200

    Initialize a database using flyway and add related entities

---
+++ src/main/java/com/interview/entity/DeviceEntity.java
@@ -0,0 +1,97 @@
+package com.interview.entity;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.interview.enums.DeviceState;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "device")
+public class DeviceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank(message = "Brand cannot be blank")
+    @Column(nullable = false)
+    private String brand;
+
+    @NotNull(message = "State cannot be null")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DeviceState state;
+
+    @CreationTimestamp
+    @Column(name = "creation_time", nullable = false, updatable = false)
+    private LocalDateTime creationTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public DeviceState getState() {
+        return state;
+    }
+
+    public void setState(DeviceState state) {
+        this.state = state;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof DeviceEntity that)) {
+            return false;
+        }
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+}
---
+++ src/main/java/com/interview/enums/DeviceState.java
@@ -0,0 +1,10 @@
+package com.interview.enums;
+
+/**
+ * Represents the state of the device.
+ */
+public enum DeviceState {
+    AVAILABLE,
+    IN_USE,
+    INACTIVE
+}
